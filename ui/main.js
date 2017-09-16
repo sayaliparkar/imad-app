2189e0fdaf89d34cd42a83c159c471b5afd42cbb
sayaliparkar <31857944+sayaliparkar@users.noreply.github.com>
2017-09-16 12:15:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,4 +5,8 @@ var element = document.getElementById('main-text');
 
 element.innertHTML = 'New value';
 
-//
+//Move the image
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.styl.marginLeft = '100px';
+};
