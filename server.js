cf5e652355d996bfec38a0cb08c63e906dae1fe4
sayaliparkar <31857944+sayaliparkar@users.noreply.github.com>
2017-09-16 13:21:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter',function(req, res) {
     counter = counter +1;
-    res.send(counter.toStrin());
+    res.send(counter.toString());
 });
 
 app.get('/ui/main.js', function (req, res) {
